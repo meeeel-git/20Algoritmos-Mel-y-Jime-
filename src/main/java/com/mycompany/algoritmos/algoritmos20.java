6699cc2dcf088d37458a98efd6411472c7dec5af
Melany Amador <melany.amadoracuna@ucr.ac.cr>
2026-09-10 16:09:57 -0600

    creación de las clases

---
+++ src/main/java/com/mycompany/algoritmos/algoritmos20.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.algoritmos;
+
+/**
+ *
+ * @author Melany Amador
+ */
+public class algoritmos20{
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
